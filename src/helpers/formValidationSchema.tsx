3b643fe4dce358784de8168d9d3cfd5765030ff2
Ivan Shishkov <ivanshishkov@MacBook-Air-Ivan.local>
2022-06-28 16:34:07 +0300

    remove second validation

--- src/helpers/formValidationSchema.tsx
+++ src/helpers/formValidationSchema.tsx
@@ -1,8 +1,7 @@
 import * as yup from 'yup';
 
 type Values = {
-  phone: string;
-  cringe: string
+  phone: string
 }
 
 const phoneRegEx = /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/
@@ -10,13 +9,6 @@ const phoneRegEx = /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-
 export const getValidationSchema = () => {
   const validationSchema = yup.object<Record<keyof Values, yup.AnySchema>>({
     phone: yup
-      .string()
-      .required('This field is required')
-      .nullable()
-      .matches(phoneRegEx, 'Phone number is not valid')
-      .min(10)
-      .max(10),
-    cringe: yup
       .string()
       .required('This field is required')
       .nullable()
